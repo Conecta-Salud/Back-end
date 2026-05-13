db528e17ff7d57277a97f3a0db05c54c010be9e5
GaelGlez <gaelos.glez@gmail.com>
2026-05-13 12:36:41 -0600

    cometntario añadido

--- src/main/java/com/itesm/interfaces/rest/DashboardResource.java
+++ src/main/java/com/itesm/interfaces/rest/DashboardResource.java
@@ -7,7 +7,7 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
+// Rama Develop
 @Path("/dashboard")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
